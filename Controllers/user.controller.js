6970e4fa8ce5d33a4501cf7076057d86aee8e898
meesahil7 <meesahil7@gmail.com>
2023-04-14 11:38:07 +0530

    updated

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -30,42 +30,28 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const user = await userModel.findOne({ email });
+    const {email, password} = req.body;
+    let user = await userModel.findOne({ email });
 
     if (!user) {
-      return res.send({ err: "User is not registered" });
+      return res.send({ message: "User not found" });
     }
 
-    if (user.lock_until !== null && Date.now() > user.lock_until) {
-      await userModel.findByIdAndUpdate(
-        { _id: user._id },
-        { $set: { suspect: false, lock_until: null, login_error: 0 } }
-      );
+    let {login_error, lock_until, suspect} = user;
+
+    if (lock_until >= Date.now()) {
+      return res.send({ message: "Try after 24 hours from your last wrong attempt" });
     }
 
-    const attempt = user.login_error + 1;
-    if (attempt >= 5) {
-      if (attempt === 5) {
-        const unlock = Date.now() + 24 * 3600 * 1000;
-        await userModel.findByIdAndUpdate(
-          { _id: user._id },
-          { $set: { lock_until: unlock, suspect: true } }
-        );
-      }
-      await userModel.findByIdAndUpdate(
-        { _id: user._id },
-        { $set: { login_error: attempt } }
-      );
-      return res.send({
-        warning: "Maximum attempt limit exceeded",
-        err: "Try after 24 hours from your last wrong attempt",
-      });
+    if (lock_until !== null && Date.now() > lock_until) {
+      await userModel.findByIdAndUpdate({_id: user._id}, {$set: {lock_until: null, login_error: 0, suspect: false}})
     }
-
+    
+    user = await userModel.findOne({ email });
+    
     const hash = user.password;
     bcrypt.compare(password, hash, async (err, result) => {
-      if (result && !user.suspect) {
+      if (result) {
         const token = jwt.sign({ user_id: user._id }, process.env.KEY);
         await userModel.findByIdAndUpdate(
           { _id: user._id },
@@ -73,16 +59,22 @@ const login = async (req, res) => {
         );
         return res.send({ message: "Logged in successfully", Token: token });
       } else {
-        const attempt = user.login_error + 1;
+        const wrong_attempt = user.login_error + 1;
         await userModel.findByIdAndUpdate(
           { _id: user._id },
-          { $set: { login_error: attempt } }
+          { $set: { login_error: wrong_attempt }}
         );
+
+        if (wrong_attempt === 5) {
+          const unlock = Date.now() + 24 * 3600 * 1000;
+          await userModel.findByIdAndUpdate({_id: user._id}, {$set: {lock_until: unlock, login_error: wrong_attempt, suspect: true}});
+          return res.send({ message: "Try after 24 hours from your last wrong attempt" });
+        }
 
         return res.send({
           err: "Wrong password",
-          total_attempts: user.login_error + 1,
-          attempts_remaining: 5 - user.login_error - 1,
+          wrong_attempts: login_error + 1,
+          attempts_remaining: 5 - login_error - 1,
         });
       }
     });
